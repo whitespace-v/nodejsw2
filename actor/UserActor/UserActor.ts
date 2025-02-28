aa67feada33ab3a089d89fefbe8076f93ded0a92
Your Name <you@example.com>
2025-02-28 14:34:40 +1000

    s

--- actor/UserActor/UserActor.ts
+++ actor/UserActor/UserActor.ts
@@ -2,25 +2,47 @@ import { Request, Response } from "express";
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 import 'dotenv/config';
+import { Responder } from "../middleware/Responder";
 
 export class UserActor {
-    static async signin(){
-
+    static async signin(req: Request, res: Response){
+        // 1. get data
+        const {login, password} = req.body
+        // 2. find User in DB by login
+        const user = {
+            id: 0,
+            login,
+            password: "$2b$05$VWC/H.674Jy1YPfQWLFjRO.zbP4BDOaEnRKRg8OeZ5EG/LRyoEJ7i"
+        }
+        if (!user){
+            res.send("Пользователь не найден")
+        } 
+        // 3. compare passwords
+        let pwdCompare = bcrypt.compareSync(password, user.password)
+        if (!pwdCompare){
+            res.send("Пароль неверный")
+        }
+        // 4. token
+        const token = jwt.sign(
+            {login: user.login},
+            process.env.SECRET_KEY || "DEFAULT_SECRET_KEY", 
+            {expiresIn: '24h'}
+        )
+        res.json({token, user})
     }
     static async signup(req: Request, res: Response){
         // 1. get data 
-        console.log(req)
         const {login, password} = req.body
         // 2. validate data
         if (!login || !password) {
-            return res.send("Некорректные данные")
+            res.send(Responder.forbidden("Некорректные данные"))
         }
         // 3. check Users on unique (DB)
         // const candidate = users.findOne({login})
         const candidate = {login: "petya", password: "vasya"}
-        if (candidate) {
-            return res.send("Логин занят")
-        }
+        // if (candidate.login) {
+        //     res.send(Responder.forbidden("Логин занят"))
+        // }
         // 4. hash pwd
         const hashPassword = await bcrypt.hash(password, 5)
         // 5. create user (DB)
@@ -31,11 +53,10 @@ export class UserActor {
         // 6. login user
         const token = jwt.sign(
             {login: user.login},
-            // @ts-expect-error
-            process.env.SECRET_KEY, 
+            process.env.SECRET_KEY || "DEFAULT_SECRET_KEY", 
             {expiresIn: '24h'}
         )
-        return res.json({token})
+        res.json(Responder.ok({token, user}))
     }
 }
 
--- router/userRouter.ts
+++ router/userRouter.ts
@@ -4,3 +4,4 @@ import { UserActor } from '../actor/UserActor/UserActor'
 export const userRouter = express.Router()
 
 userRouter.post('/signup', UserActor.signup)
+userRouter.post('/signin', UserActor.signin)
